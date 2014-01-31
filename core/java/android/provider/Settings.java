804c0a35a37095a76aae103ac0dd3cc86550cbff
GwonHyeok <kh4975@naver.com>
2014-01-31 09:40:00 +0900

    StatusBar Color Setting: Set Data Text Color
    Change-Id: I6c002eedf16b5d4dd56e9145f5ed521bf900b017

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3179,7 +3179,7 @@ public final class Settings {
         */
         public static final String STATUS_BAR_DATA_COLOR = "status_bar_data_color";
 
-        /**
+	/**
         * Statusbar Airplain Color
         * @hide
         */
