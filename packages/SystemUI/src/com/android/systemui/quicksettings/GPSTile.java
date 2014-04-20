481b5abb0549bcab770127752bf2c459519b80b5
copycat <dskim9752@gmail.com>
2014-04-20 12:03:28 +0900

    fix
    중복붕복

--- packages/SystemUI/src/com/android/systemui/quicksettings/GPSTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/GPSTile.java
@@ -72,29 +72,25 @@ public class GPSTile extends QuickSettingsTile implements LocationSettingsChange
     }
 
     private synchronized void updateTile() {
-        int textResId;
-        switch(mCurrentMode) {
-        case Settings.Secure.LOCATION_MODE_SENSORS_ONLY:
-            textResId = R.string.location_mode_sensors_only_title;
-            mDrawable = R.drawable.ic_qs_location_on;
-            break;
-        case Settings.Secure.LOCATION_MODE_BATTERY_SAVING:
-            textResId = R.string.location_mode_battery_saving_title;
-            mDrawable = R.drawable.ic_qs_location_lowpower;
-            break;
-        case Settings.Secure.LOCATION_MODE_HIGH_ACCURACY:
-            textResId = R.string.location_mode_high_accuracy_title;
-            mDrawable = R.drawable.ic_qs_location_on;
-            break;
-        default:
-            textResId = R.string.quick_settings_location_off_label;
-            mDrawable = R.drawable.ic_qs_location_off;
-            break;
+        switch (mLocationMode) {
+            case Settings.Secure.LOCATION_MODE_SENSORS_ONLY:
+                mDrawable = R.drawable.ic_qs_location_on_gps;
+                break;
+            case Settings.Secure.LOCATION_MODE_BATTERY_SAVING:
+                mDrawable = R.drawable.ic_qs_location_on_wifi;
+                break;
+            case Settings.Secure.LOCATION_MODE_HIGH_ACCURACY:
+                mDrawable = R.drawable.ic_qs_location_on;
+                break;
+            case Settings.Secure.LOCATION_MODE_OFF:
+                mDrawable = R.drawable.ic_qs_location_off;
+                break;
         }
         int textResId = mLocationEnabled ? R.string.quick_settings_location_label
                 : R.string.quick_settings_location_off_label;
         mLabel = mContext.getText(textResId).toString();
     }
+
 
     @Override
     public void onLocationSettingsChanged(boolean locationEnabled, int locationMode) {
